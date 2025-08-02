d86a09e146d57f30349154f03324f4028cf88c7a
Vinor <vinorval@yandex.ru>
2025-08-02 11:56:32 +0300

    add for navigations block styles

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,15 +3,15 @@ import navigationLinks from "../../utils/navigationLinks";
 
 const Navigation = () => {
     return (
-    <div className="navigation-block" >
+    <div className="navigation-block padding_limit_width background_color_gray" >
         <nav className="navigation" >
-            <ul className="navigation__list" >
+            <ul className="navigation__list margin_none padding_none" >
                 {navigationLinks.map((el) => (<li className="navigation__link" >{el.name}</li>))}
             </ul>
         </nav>
         <div className="block-call-back" >
-            <p className="block-call-back__number" >924-565-9456</p>
-            <button className="block-call-back__button" >Заказать обратный звонок</button>
+            <p className="block-call-back__number margin_none" >924-565-9456</p>
+            <button className="block-call-back__button margin_none padding_none background_none" >Заказать обратный звонок</button>
         </div>
     </div>
 );
