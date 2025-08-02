b4745a87b0a9bf5983780c20beb77280765a3b04
Vinor <vinorval@yandex.ru>
2025-08-02 12:17:31 +0300

    cleane file App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,22 +4,8 @@ import Header from "../header/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navigation />
-        <Header />
-        <img  className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Header />
     </div>
   );
 }
