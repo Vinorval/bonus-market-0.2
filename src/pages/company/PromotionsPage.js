99b83bd33f6256a00f88ca16c8f65b63ac13cb87
Vinor <vinorval@yandex.ru>
2025-08-09 11:24:59 +0300

    fix: change items on pages promo and news

--- src/pages/company/PromotionsPage.js
+++ src/pages/company/PromotionsPage.js
@@ -1,8 +1,7 @@
 import Location from "../../components/location/Location";
 import Nav from "../../components/nav/Nav";
 import { navPagesCompanyArr } from "../../utils/navigationPage";
-
-import plombir from "../../images/promotions/plombir.jpg";
+import { promoArr } from "../../utils/promoAndNews";
 
 const PromotionsPage = () => {
     const location = [ { link: "/", text: 'Главная ' }, { link: "/company", text: '/ О компании ' }, { link: "/promotions", text: '/ Акции ' } ];
@@ -17,15 +16,17 @@ const PromotionsPage = () => {
                     <h3 className="info-block__title margin_none info-block__title_width_full">Все акции</h3>
                 </div>
                 <ul className="margin_none padding_none news-list">
-                    <li className="margin_none list-style_none news-card">
-                        <img src={plombir} alt="" />
-                        <div className="news-card__info">
-                            <p className="margin_none">Новое поступление</p>
-                            <span>05.04.2020</span>
-                            <p className="margin_none">Знаменитый газированный напиток Dr Pepper производится корпорацией Dr Pepper Snapple Group Company (бывшее название компании – Cadbury Schweppes Americas Beverages). Помимо самого своего известного бренда компания выпускает и другие газированные и негазированные напитки, чаи, миксы, даже пиво. Под этой маркой выпускается Schweppes, A&W Root Beer, Hawaiian Punch, Motts. Основной рынок сбыта — страны Северной Америки.</p>
-                            <button className="news-card__button button_color_yellow button_width_narrow margin_none padding_none">Подробнее</button>
-                        </div>
-                    </li>
+                    {promoArr.map((el) => (
+                        <li key={el.id} className="margin_none list-style_none news-card">
+                            <img src={el.img} alt={el.name} />
+                            <div className="news-card__info">
+                                <p className="margin_none">{el.name}</p>
+                                <span>{el.date}</span>
+                                <p className="margin_none">{el.description}</p>
+                                <button className="news-card__button button_color_yellow button_width_narrow margin_none padding_none">Подробнее</button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </section>
         </main>
---
+++ src/utils/promoAndNews.js
@@ -0,0 +1,71 @@
+import plombir from '../images/promotions/plombir.jpg';
+import marka from '../images/promotions/marka.jpg';
+import sirovbogatov from '../images/promotions/sirobogatov.jpg';
+import ice from '../images/promotions/ice.jpg';
+
+import pelmen from '../images/news/pelmen.jpg';
+import swejaya from '../images/news/swejaya.jpg';
+import cheese from '../images/news/cheese.jpg';
+import cola from '../images/news/cola.jpg';
+
+export const newsArr = [
+    {
+        id: '001',
+        img: pelmen,
+        name: 'Новое поступление',
+        date: '05.04.2020',
+        description: '',
+    },
+    {
+        id: '002',
+        img: swejaya,
+        name: 'Свежая марка',
+        date: '05.04.2020',
+        description: '',
+    },
+    {
+        id: '003',
+        img: cheese,
+        name: 'Новинка',
+        date: '05.04.2020',
+        description: '',
+    },
+    {
+        id: '004',
+        img: cola,
+        name: 'DR PEPPER',
+        date: '05.04.2020',
+        description: 'Знаменитый газированный напиток Dr Pepper производится корпорацией Dr Pepper Snapple Group Company (бывшее название компании – Cadbury Schweppes Americas Beverages). Помимо самого своего известного бренда компания выпускает и другие газированные и негазированные напитки, чаи, миксы, даже пиво. Под этой маркой выпускается Schweppes, A&W Root Beer, Hawaiian Punch, Motts. Основной рынок сбыта — страны Северной Америки.',
+    },
+];
+
+export const promoArr = [
+    {
+        id: '001',
+        img: plombir,
+        name: 'Молочко',
+        date: '05.04.2020',
+        description: '',
+    },
+    {
+        id: '002',
+        img: marka,
+        name: 'Свежая марка',
+        date: '05.04.2020',
+        description: 'Cыр торговой марки «Сыробогатов» — наша визитная карточка, наша гордость! Мы ведем тщательный контроль качества каждого сорта сыра, и только самые достойные попадают под бренд «Сыробогатов». Это те сыры, в которые мы с радостью вкладываем свою энергию, любовь, время. Новая современная упаковка, высокий уровень узнаваемости бренда, отменный вкус каждого кусочка — это и есть гарантия успеха!',
+    },
+    {
+        id: '003',
+        img: sirovbogatov,
+        name: 'Сыробогатов',
+        date: '05.04.2020',
+        description: 'Cыр торговой марки «Сыробогатов» — наша визитная карточка, наша гордость! Мы ведем тщательный контроль качества каждого сорта сыра, и только самые достойные попадают под бренд «Сыробогатов». Это те сыры, в которые мы с радостью вкладываем свою энергию, любовь, время. Новая современная упаковка, высокий уровень узнаваемости бренда, отменный вкус каждого кусочка — это и есть гарантия успеха!',
+    },
+    {
+        id: '004',
+        img: ice,
+        name: 'Мороженное',
+        date: '05.04.2020',
+        description: '',
+    },
+];
