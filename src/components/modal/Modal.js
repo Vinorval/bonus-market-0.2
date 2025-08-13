bd462307837ff3b0d67b1932d352840f6f31a1b3
Vinor <vinorval@yandex.ru>
2025-08-13 11:41:00 +0300

    feat: create modal type wrraning

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Close from '../../images/close.svg';
 
-const Modal = ({ show, closePopup, children }) => {
+const Modal = ({ show, closePopup, children, type }) => {
     //закрытие модала на esc
     React.useEffect(() => {
         const close = (e) => { if(e.key === 'Escape') closePopup()};
         document.addEventListener('keydown', close);
-
         return () => document.removeEventListener('keydown', close);
     });
 
     return (
-        <section className={ show ? 'popup' : 'invisible'} onClick={e => (e.currentTarget === e.target) && closePopup()} >
+        <section className={ show ? `popup popup_show_active popup_type_${type}` : `popup popup_type_${type}`} onClick={e => (e.currentTarget === e.target) && closePopup()} >
             <div className='popup__wrap' >
-                <button className='popup__buttonClose' onClick={closePopup} >
-                    <img src={Close} className='popup__ikonClose' alt='Иконка закрытия попапа'/>
+                <button className='popup__button-close' onClick={closePopup} >
+                    <img src={Close} className='popup__ikon-close' alt='Иконка закрытия попапа'/>
                 </button>
-                {children}
+                <div className='popup__children'>{children}</div>
             </div>
         </section>
     )
--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,4 +1,6 @@
+import React from "react";
 import AuthForm from "../../components/authForm/AuthFrom";
+import Modal from "../../components/modal/Modal";
 
 const LoginPage = () => {
     const formObject = {
@@ -13,6 +15,8 @@ const LoginPage = () => {
             { text: 'Забыли пароль?', path: '/forgot-password', link: 'Восстановить пароль' },
         ],
     };
+    const [ showModal, setShowModal ] = React.useState(true);
+    const onClick = () => setShowModal(!showModal);
 
     return (
         <main className="page__auth padding_limit_width">
@@ -22,6 +26,10 @@ const LoginPage = () => {
                 button={formObject.button} 
                 linksArr={formObject.linksArr} 
             />
+            <Modal show={showModal} closePopup={onClick} type='warning'>
+                <h3 className="margin_none">Просьба не вносить настоящие данные.</h3>
+                <p className="margin_none">Данный сайт - дэмо. Он работает без сервера, а все данные сохраняются в кэше вашего браузера.</p>
+            </Modal>
         </main>
     )
 };
