926c7a1e5901a5bca2b84024c979c1d7a7e75f54
Vinor <vinorval@yandex.ru>
2025-08-18 12:28:05 +0300

    add draft version of the array

---
+++ src/utils/products.js
@@ -0,0 +1,71 @@
+import milkDay from "../images/products/milk/milk-day.jpg";
+import milkTyagen from "../images/products/milk/milk-tyagin.jpg";
+
+const products = [
+    {
+        _id: '001',
+        name: 'Молоко Арта 2,5%День 1 л 1/12 (6мес)',
+        img: {milkDay},
+        category: ['milk', 'milk'],
+        price: '64.20',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '6 мес', 'КОПМАНИЯ': 'Арта' }
+    },
+    {
+        _id: '002',
+        name: 'Молоко ультрапаст. "Тяжин" 2.5% тетра пак1л 1/12 (9мес)',
+        img: {milkTyagen},
+        category: ['milk', 'milk'],
+        price: '64.10',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '9 мес', 'КОПМАНИЯ': 'Тяжин' }
+    },
+    {
+        _id: '003',
+        name: 'Молоко Арта 2,5%День 1 л 1/12 (6мес)',
+        img: {milkDay},
+        category: ['milk', 'milk'],
+        price: '64.20',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '6 мес', 'КОПМАНИЯ': 'Арта' }
+    },
+    {
+        _id: '004',
+        name: 'Молоко ультрапаст. "Тяжин" 2.5% тетра пак1л 1/12 (9мес)',
+        img: {milkTyagen},
+        category: ['milk', 'milk'],
+        price: '64.10',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '9 мес', 'КОПМАНИЯ': 'Тяжин' }
+    },
+    {
+        _id: '005',
+        name: 'Молоко Арта 2,5%День 1 л 1/12 (6мес)',
+        img: {milkDay},
+        category: ['milk', 'milk'],
+        price: '64.20',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '6 мес', 'КОПМАНИЯ': 'Арта' }
+    },
+    {
+        _id: '006',
+        name: 'Молоко ультрапаст. "Тяжин" 2.5% тетра пак1л 1/12 (9мес)',
+        img: {milkTyagen},
+        category: ['milk', 'milk'],
+        price: '64.10',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '9 мес', 'КОПМАНИЯ': 'Тяжин' }
+    },
+    {
+        _id: '007',
+        name: 'Молоко Арта 2,5%День 1 л 1/12 (6мес)',
+        img: {milkDay},
+        category: ['milk', 'milk'],
+        price: '64.20',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '6 мес', 'КОПМАНИЯ': 'Арта' }
+    },
+    {
+        _id: '008',
+        name: 'Молоко ультрапаст. "Тяжин" 2.5% тетра пак1л 1/12 (9мес)',
+        img: {milkTyagen},
+        category: ['milk', 'milk'],
+        price: '64.10',
+        characteristic: { 'ОБЪЕМ': '1 л', '%': '2.5%', 'СРОК ГОДНОСТИ': '9 мес', 'КОПМАНИЯ': 'Тяжин' }
+    },
+];
+
+export default products;
